f44e6e2c0348aa00391baceb049d5f88be71b116
chenwenchang <2474580493@qq.com>
2024-07-12 16:26:25 +0800

    1st vue3

---
+++ calculate-vue3/calculate-vue3/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed} from 'vue'
+import { ref } from 'vue'
+const add = computed(()=>{
+  const num1=ref()
+  const num2=ref()
+  return num1.value+num2.value
+})
+</script>
+
+<template>
+  <header>
+<input type="number" v-model="num1">
++
+<input type="number" v-model="num2">
+=
+<span>{{ add }}</span>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
